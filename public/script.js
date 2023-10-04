8c152b6ac609431a534ec282b8090304dcfbfa35
Mahmoud Hmmadia <102804429+MahmoudHmmadia@users.noreply.github.com>
2023-10-04 10:06:36 +0300

    f

--- public/script.js
+++ public/script.js
@@ -14,7 +14,7 @@ document.querySelector(".btn").addEventListener("click", () => {
       })
       .then((stream) => {
         const myPeer = new Peer(undefined, {
-          host: "task-2-om2k.onrender",
+          host: "task-2-om2k.onrender/peerjs",
           port: "3001",
         });
         myPeer.on("open", (id) => {
