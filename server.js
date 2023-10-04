301a2bad07cc9d8e6ae2aa096db6287535b21fbc
Mahmoud Hmmadia <102804429+MahmoudHmmadia@users.noreply.github.com>
2023-10-04 08:53:03 +0300

    f

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { Server } from "socket.io";
+import { v4 } from "uuid";
+import http from "http";
+const app = express();
+app.set("view engine", "ejs");
+const server = http.Server(app);
+const io = new Server(server, {
+  cors: {
+    origin: true,
+    methods: ["GET", "POST"],
+  },
+});
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.redirect(`/${v4()}`);
+});
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+io.on("connection", (socket) => {
+  socket.on("join-room", (rId, uId) => {
+    socket.join(rId);
+    socket.to(rId).emit("user-connected", uId);
+    socket.on("disconnect", () => {
+      socket.to(rId).emit("user-disconnected", uId);
+    });
+  });
+});
+server.listen(3000);
