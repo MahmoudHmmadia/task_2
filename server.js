591edcbe0762a580181a0538075d460855772dbb
Mahmoud Hmmadia <102804429+MahmoudHmmadia@users.noreply.github.com>
2023-10-08 12:28:20 +0300

    dd

--- server.js
+++ server.js
@@ -45,7 +45,6 @@ const expressPeerServer = ExpressPeerServer(server, {
   },
 });
 app.use(expressPeerServer);
-app.use("/peerjs", expressPeerServer);
 
 server.listen(process.env.PORT || 3002, () => {
   console.log("asd");
