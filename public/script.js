79e548ddff3ca83c2655070b7e28e73af4da243b
Mahmoud Hmmadia <102804429+MahmoudHmmadia@users.noreply.github.com>
2023-10-09 09:52:29 +0300

    asd

--- public/script.js
+++ public/script.js
@@ -21,24 +21,28 @@ document.querySelector(".btn").addEventListener("click", () => {
           secure: true,
           config: {
             iceServers: [
-              { url: "stun:stun01.sipphone.com" },
-              { url: "stun:stun.ekiga.net" },
-              { url: "stun:stunserver.org" },
-              { url: "stun:stun.softjoys.com" },
-              { url: "stun:stun.voiparound.com" },
-              { url: "stun:stun.voipbuster.com" },
-              { url: "stun:stun.voipstunt.com" },
-              { url: "stun:stun.voxgratia.org" },
-              { url: "stun:stun.xten.com" },
               {
-                url: "turn:192.158.29.39:3478?transport=udp",
-                credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-                username: "28224511:1379330808",
+                urls: "stun:stun.relay.metered.ca:80",
               },
               {
-                url: "turn:192.158.29.39:3478?transport=tcp",
-                credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-                username: "28224511:1379330808",
+                urls: "turn:a.relay.metered.ca:80",
+                username: "976776674d5e26c0b97dd685",
+                credential: "dqwWyHCY+eCXcn8I",
+              },
+              {
+                urls: "turn:a.relay.metered.ca:80?transport=tcp",
+                username: "976776674d5e26c0b97dd685",
+                credential: "dqwWyHCY+eCXcn8I",
+              },
+              {
+                urls: "turn:a.relay.metered.ca:443",
+                username: "976776674d5e26c0b97dd685",
+                credential: "dqwWyHCY+eCXcn8I",
+              },
+              {
+                urls: "turn:a.relay.metered.ca:443?transport=tcp",
+                username: "976776674d5e26c0b97dd685",
+                credential: "dqwWyHCY+eCXcn8I",
               },
             ],
           },
